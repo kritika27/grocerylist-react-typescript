787a408bfc433c84f6d58433ef54db30dba7972f
kritika <kritikasri27@gmail.com>
2021-04-03 00:58:58 +0530

    Added typescript logic to Grocery List App in react

---
+++ src/App.tsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import "./App.css";
+import Item from "./components/Item";
+
+ interface NewItem {
+  id: number;
+  item: string;
+  complete: boolean;
+};
+
+const App: React.FC =()=>{
+  const [item, setItem] = useState<string>("");
+  const [list, setList] = useState<NewItem[]>([]);
+
+  const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+    const newItem = {
+      id: Math.random(),
+      item: item,
+      complete: false,
+    };
+    e.preventDefault();
+    if (item && item.length <= 25) {
+      setList([...list, newItem]);
+      setItem("");
+    }
+  };
+
+  React.useEffect(() => {
+    localStorage.setItem("data", JSON.stringify(list));
+  }, [list]);
+
+  const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setItem(e.target.value);
+  };
+
+  const remove = (id:number): void => {
+    setList(list.filter((el) => el.id !== id));
+  };
+  const handleComplete = (id:number):void => {
+    setList(
+      list.map((item) => {
+        if (item.id === id) {
+          return {
+            ...item,
+            complete: !item.complete,
+          };
+        }
+        return item;
+      })
+    );
+  };
+
+  return (
+    <div className="App">
+      <h1>Grocery List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="input"
+          type="text"
+          value={item}
+          placeholder="Enter the items"
+          onChange={handleChange}
+        />
+        <button className="btn" type="submit">
+          Add Items
+        </button>
+        <br></br>
+        <br></br>
+      </form>
+      <div>
+        {list.map((c, id) => (
+          <Item
+            key={id}
+            id={c.id}
+            item={c.item}
+            
+            complete={c.complete}
+            
+            remove={remove}
+            handleComplete={handleComplete}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Item.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Item.css";
+
+interface FuncProps{
+    remove(id:number):void;
+    handleComplete(id:number):void;
+    id:number;
+    item:string;
+    complete:boolean;
+}
+
+const Item :React.FC<FuncProps> = ({ id, item, remove,handleComplete,complete }:FuncProps)  => {
+  
+  return (
+    <div className="item">
+      <input
+        type="text"
+        value={item}
+        style={{
+          border: "none",
+          outline: "none",
+          backgroundColor: "transparent",
+          color: "white",
+          fontSize: "20px",
+        }}
+        
+        className={complete ? "complete" : ""}
+      />
+      <img
+        onClick={() => handleComplete(id)}
+        src="https://img.icons8.com/offices/40/000000/checked-2--v2.png"
+        alt="complete task"
+      />
+      <img
+        onClick={() => remove(id)}
+        src="https://img.icons8.com/color/48/000000/trash.png"
+        alt="Delete"
+      />
+    </div>
+  );
+};
+export default Item;
